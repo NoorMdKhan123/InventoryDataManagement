9990804753f764727f9b6dfc696a25661067f736
NoorMdKhan123 <86184257+NoorMdKhan123@users.noreply.github.com>
2021-10-05 17:20:57 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,8 +17,7 @@ namespace InventoryDataManagement
                 Console.WriteLine(data.TypesOfRice[i].name);
                 Console.WriteLine(data.TypesOfRice[i].weight);
                 Console.WriteLine(data.TypesOfRice[i].Price);
-                Evaluation.InventoryEvaluation(data.TypesOfRice[i].name,
-                    data.TypesOfRice[i].weight, data.TypesOfRice[i].Price);
+               
                 Console.WriteLine("------");
             }
 
@@ -28,8 +27,7 @@ namespace InventoryDataManagement
                 Console.WriteLine(data.TypesOfPulses[i].name);
                 Console.WriteLine(data.TypesOfPulses[i].Price);
                 Console.WriteLine(data.TypesOfPulses[i].weight);
-                Evaluation.InventoryEvaluation(data.TypesOfPulses[i].name,
-                    data.TypesOfPulses[i].weight, data.TypesOfPulses[i].Price);
+                
                 Console.WriteLine("------");
             }
 
@@ -39,8 +37,7 @@ namespace InventoryDataManagement
                 Console.WriteLine(data.TypesOfWheats[i].name);
                 Console.WriteLine(data.TypesOfWheats[i].Price);
                 Console.WriteLine(data.TypesOfWheats[i].weight);
-                Evaluation.InventoryEvaluation(data.TypesOfWheats[i].name,
-                   data.TypesOfWheats[i].weight, data.TypesOfWheats[i].Price);
+                
                 Console.WriteLine("------");
             }
         }
