4a25115e9be8cea96c6b3b483e14e06f594fc8e4
NoorMdKhan123 <86184257+NoorMdKhan123@users.noreply.github.com>
2021-10-05 16:10:06 +0530

    Update TypesOfPulses.cs

--- TypesOfPulses.cs
+++ TypesOfPulses.cs
@@ -7,7 +7,7 @@ namespace InventoryDataManagement
     class TypesOfPulses
     {
         public string name;
-        public string weight;
+        public int weight;
         public int Price;
 
 
